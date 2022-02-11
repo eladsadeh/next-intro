0605d5d6feb6127161a4f1bb101d9eed311feb62
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-11 17:32:01 -0500

    add rates and jsx style

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 const About = () => (
 	<Layout>
 		<h1>This is the about page</h1>
-		
+		<style jsx>{`
+			h1 {
+				font-family: 'Open Sans';
+				color: grey;
+			}
+		`}</style>
 	</Layout>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import fetch from 'isomorphic-unfetch'
-import Layout from '../components/Layout'
+import Fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
+import Rates from '../components/Rates';
 
-const Index = () => (
+const Index = (props) => (
 	<Layout>
-		<h1>First Next App!</h1>
+		<h1>Currency Exchange Rates</h1>
+		<Rates rates={props.rates} />
+		<style jsx>{`
+			h1 {
+				font-family: 'Open Sans';
+				color: grey;
+			}
+		`}</style>
 	</Layout>
 );
 
 Index.getInitialProps = async () => {
 	const res = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
-	const data = await res.json();
+	const rates = await res.json();
+	console.log('props:', rates);
 	return {
-		data: data,
+		rates: rates,
 	};
 };
 
