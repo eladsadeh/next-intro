49728b9696273b1a2179091fc36cbddad4ac2c8e
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-11 17:05:48 -0500

    install isomorphic-unfetch

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-unfetch'
 import Layout from '../components/Layout'
 
 const Index = () => (
@@ -5,5 +6,13 @@ const Index = () => (
 		<h1>First Next App!</h1>
 	</Layout>
 );
+
+Index.getInitialProps = async () => {
+	const res = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+	const data = await res.json();
+	return {
+		data: data,
+	};
+};
 
 export default Index;
